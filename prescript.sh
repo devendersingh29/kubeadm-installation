f0eab80ebeda325a2a475b5595c67aaa9aad3e55
ddevender29 <devender.singh@walkingtree.tech>
2026-06-19 12:37:19 +0530

    script

---
+++ prescript.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Update package index and install required packages
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl
+
+# Add Docker's official GPG key
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add Docker repository to Apt sources
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl enable docker
+#sudo systemctl status docker
+
+# Show disk space
+df -h
+
+# Add Kubernetes GPG key and repository
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+# Install Kubernetes components
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo systemctl enable --now kubelet
+sudo apt-mark hold kubelet kubeadm kubectl
+
+# Disable swap
+sudo swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
+# Load necessary kernel modules
+echo -e "overlay\nbr_netfilter" | sudo tee /etc/modules-load.d/containerd.conf
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# Configure sysctl settings for Kubernetes
+echo -e "net.bridge.bridge-nf-call-ip6tables = 1\nnet.bridge.bridge-nf-call-iptables = 1\nnet.ipv4.ip_forward = 1" | sudo tee /etc/sysctl.d/kubernetes.conf
+sudo sysctl --system
+
+# Stop AppArmor and restart containerd service
+sudo systemctl stop apparmor
+sudo systemctl restart containerd.service
+
+# Check connectivity to the Kubernetes API server
+#telnet 192.168.1.62 6443
+
+# Check the status of containerd
+#sudo systemctl status containerd
+
+# Clear containerd config file
+sudo sh -c 'echo "" > /etc/containerd/config.toml'
+
+# Restart containerd and check its status
+sudo systemctl restart containerd
+#sudo systemctl status containerd
+
+ 
